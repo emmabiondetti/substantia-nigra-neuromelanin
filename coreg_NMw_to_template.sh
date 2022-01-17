6f9bf6b1f7e3e8caa2e0ea3f9c09ba77608353ed
Emma Biondetti <emma.biondetti@gmail.com>
2022-01-17 12:43:16 +0100

    Update coreg_NMw_to_template.sh
    Adding .gz extension to all .nii files

--- coreg_NMw_to_template.sh
+++ coreg_NMw_to_template.sh
@@ -10,7 +10,7 @@
 #Date: 17/01/2021
 
 #Defining brain template file
-brain_template=average_nonlin_10.nii
+brain_template=average_nonlin_10.nii.gz
 
 #Definining and creating output directories
 outDir=Coregistrations_NMw_to_T1w
@@ -38,16 +38,16 @@ reg_aladin -flo $NMw_img -ref $T1w_img -aff $outDir/rT1_NMw_mat_rigid.txt -rigOn
 #This step is necessary for neuromelanin-sensitive turbo spin echo images, as
 #these are usually acquired with a partial field of view along the foot-head
 #direction
-reg_resample -flo $NMw_img -ref $T1w_img -aff $outDir/rT1_NMw_mat_rigid.txt -res $outDir/rT1_NMw_rigid.nii
+reg_resample -flo $NMw_img -ref $T1w_img -aff $outDir/rT1_NMw_mat_rigid.txt -res $outDir/rT1_NMw_rigid.nii.gz
 
 #Because manual SN segmentations are binary, the nearest neighbour interpolation
 #option is selected for applying the transformation to SN_ROI (-inter 0)
-reg_resample -flo $SN_ROI -ref $T1w_img -aff $outDir/rT1_NMw_mat_rigid.txt -res $outDir/rT1_SN_ROI.nii -inter 0
+reg_resample -flo $SN_ROI -ref $T1w_img -aff $outDir/rT1_NMw_mat_rigid.txt -res $outDir/rT1_SN_ROI.nii.gz -inter 0
 
 #Calculating the affine+nonlinear transformation from T1w to template
-reg_aladin -flo $brain_T1w_img -ref $brain_template -aff $outDir2/rSymmTemplate_T1w_mat_affine.txt -res $outDir2/rSymmTemplate_T1w_affine.nii
-reg_f3d -flo $brain_T1w_img -ref $brain_template -aff $outDir2/rSymmTemplate_T1w_mat_affine.txt -res $outDir2/rSymmTemplate_T1w_nonlinear.nii -cpp $outDir2/rSymmTemplate_T1w_cpp_nonlinear.nii
+reg_aladin -flo $brain_T1w_img -ref $brain_template -aff $outDir2/rSymmTemplate_T1w_mat_affine.txt -res $outDir2/rSymmTemplate_T1w_affine.nii.gz
+reg_f3d -flo $brain_T1w_img -ref $brain_template -aff $outDir2/rSymmTemplate_T1w_mat_affine.txt -res $outDir2/rSymmTemplate_T1w_nonlinear.nii.gz -cpp $outDir2/rSymmTemplate_T1w_cpp_nonlinear.nii.gz
 
 #Applying the T1w-to-template transformation to the TSE image and the segmentations (in T1w space)
-reg_resample -ref $brain_template -flo $outDir/rT1_NMw_rigid.nii -cpp $outDir2/rSymmTemplate_T1w_cpp_nonlinear.nii -res $outDir3/rSymmTemplate_rT1_NMw_nonlinear.nii
-reg_resample -ref $brain_template -flo $outDir/rT1_SN_ROI.nii -cpp $outDir2/rSymmTemplate_T1w_cpp_nonlinear.nii -res $outDir3/rSymmTemplate_rT1_SN_ROI.nii -inter 0
+reg_resample -ref $brain_template -flo $outDir/rT1_NMw_rigid.nii.gz -cpp $outDir2/rSymmTemplate_T1w_cpp_nonlinear.nii.gz -res $outDir3/rSymmTemplate_rT1_NMw_nonlinear.nii.gz
+reg_resample -ref $brain_template -flo $outDir/rT1_SN_ROI.nii.gz -cpp $outDir2/rSymmTemplate_T1w_cpp_nonlinear.nii.gz -res $outDir3/rSymmTemplate_rT1_SN_ROI.nii.gz -inter 0
